5fe462b9b787a6846d511df93417b1ff121ea9cf
Jorin <jorin.s@protonmail.com>
2023-12-19 23:00:05 +0100

    Do not wrap templates

--- theme/templates/Page.tsx
+++ theme/templates/Page.tsx
@@ -1,9 +1,9 @@
 import type { Template } from '../templates'
 
 const Page: Template = ({children}) => (
-  <div>
+  <>
     {children}
-  </div>
+  </>
 )
 
 export default Page
--- theme/templates/Tag.tsx
+++ theme/templates/Tag.tsx
@@ -1,9 +1,9 @@
 import type { Template } from '../templates'
 
 const Tag: Template = ({children}) => (
-  <div>
+  <>
     {children}
-  </div>
+  </>
 )
 
 export default Tag
