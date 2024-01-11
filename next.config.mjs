99e97113f3be7b34db8aca873774464a9f1e3bcf
Jorin <jorin.s@protonmail.com>
2024-01-11 17:47:42 +0100

    Highlight section by target

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 import nextra from 'nextra'
+import sectionize from 'remark-sectionize'
 
 /**
  * @type { import('next').NextConfig }
@@ -12,6 +13,9 @@ const nextraConfig = {
   theme: './theme/Layout.tsx',
   themeConfig: './theme.config.tsx',
   latex: true,
+  mdxOptions: {
+    remarkPlugins: [sectionize],
+  },
 }
 
 export default nextra(nextraConfig)(nextConfig)
